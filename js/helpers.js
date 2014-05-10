f1a02f7ec7a957771db0ecf2acc26b94dfe98541
Richard Nguyen <richard_nguyen@brown.edu>
2014-05-09 21:29:27 -0400

    messy reset

---
+++ js/helpers.js
@@ -0,0 +1,34 @@
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+Array.prototype.shuffle = function (){ //v1.0
+    for(var j, x, i = this.length; i; j = Math.floor(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+    return this;
+};
+
+String.prototype.hashCode = function(){
+	// See http://www.cse.yorku.ca/~oz/hash.html		
+	var hash = 5381;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)+hash) + char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+}
+
+Number.prototype.mod = function(n) {
+	return ((this%n)+n)%n;
+}
+
+$.fn.wysiwygEvt = function () {
+    return this.each(function () {
+        var $this = $(this);
+        var htmlold = $this.html();
+        $this.bind('blur keyup paste copy cut mouseup', function () {
+            var htmlnew = $this.html();
+            if (htmlold !== htmlnew) {
+                $this.trigger('change')
+            }
+        })
+    })
+}
